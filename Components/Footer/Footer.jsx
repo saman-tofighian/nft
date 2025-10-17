768ca62ead944b1c69fd9b6c350c55f090f161a6
Saman Tofighian <samantofighian78@gmail.com>
2025-10-17 16:12:54 +0330

    start header

---
+++ Components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className='w-full'>
+      <div className='grid grid-cols-12 px-[6%] w-full'></div>
+      <div className='bg-[#1E1B33] px-[6%] w-full'></div>
+    </footer>
+  );
+}
