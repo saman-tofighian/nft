eb93ad6aca2bab30d3dde7ca84a1fbd748e3ecb1
Saman Tofighian <samantofighian78@gmail.com>
2025-10-28 21:09:52 +0330

    responsive brands section

--- Components/Brands/Brands.jsx
+++ Components/Brands/Brands.jsx
@@ -2,26 +2,16 @@ import Image from 'next/image';
 
 export default function Brands() {
   const brandImage = [
-    {
-      id: 1,
-      img: '/img/b1.png',
-    },
-    {
-      id: 2,
-      img: '/img/b2.png',
-    },
-    {
-      id: 3,
-      img: '/img/b3.png',
-    },
-    {
-      id: 4,
-      img: '/img/b4.png',
-    },
+    { id: 1, img: '/img/b5.png' },
+    { id: 2, img: '/img/b1.png' },
+    { id: 3, img: '/img/b2.png' },
+    { id: 4, img: '/img/b3.png' },
+    { id: 5, img: '/img/b4.png' },
   ];
+
   return (
-    <section className='mt-14 w-full'>
-      <div className='gap-x-7 grid grid-cols-12 px-[6%] w-full'>
+    <section className='flex justify-center mt-30 w-full'>
+      <div className='justify-items-center items-center gap-y-12 sm:gap-x-12 xl:gap-y-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 px-6 w-full max-w-[1100px]'>
         {brandImage.map((val) => (
           <figure key={val.id}>
             <Image
@@ -29,7 +19,7 @@ export default function Brands() {
               alt='brand'
               width={208}
               height={59}
-              className='opacity-80 hover:opacity-100 object-contain transition-all'
+              className='h-[59px] object-center transition-all'
             />
           </figure>
         ))}
